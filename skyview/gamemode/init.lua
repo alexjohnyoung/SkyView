f39ccab675226eb0437de47493badf161189657b
tyguy550 <alexjohnyoung@gmail.com>
2015-08-05 06:19:45 +0100

    Update

--- skyview/gamemode/init.lua
+++ skyview/gamemode/init.lua
@@ -55,6 +55,8 @@ local Models =
 
 function GM:PlayerInitialSpawn(ply)
 	ply:SetModel(table.Random(Models))
+	ply.ShieldMade = false 
+	ply.Shield = nil 
 	ply.FileID = ply:SteamID():gsub(":", "-")
 	if !SkyView:PlayerExists(ply) then 
 		--player doesn't exist 
@@ -65,8 +67,60 @@ end
 
 
 function GM:KeyPress(ply, key)
-	if key == IN_ATTACK then 
-		local prop = ents.Create("prop_physics")
+	if ply:Alive() then
+		if key == IN_ATTACK and !ply.ShieldMade then 
+			local prop = ents.Create("prop_physics")
+			local pos = ply:GetPos()
+			local forward = ply:GetForward()
+			prop:SetPos(ply:EyePos() + ply:GetVelocity()*0.1 + ply:GetForward()*50)
+			prop:SetModel("models/Combine_Helicopter/helicopter_bomb01.mdl")
+			prop:SetColor(Color(0, 0, 0))
+			prop:Spawn()
+			prop:AddCallback("PhysicsCollide", function(prop, data)
+				local ent = data.HitEntity 
+				if !ent:IsWorld() then 
+					if ent.MeShield then 
+						print("DONE")
+						local OldVelocity = data.OurOldVelocity
+						local LastSpeed = math.max(OldVelocity:Length(), data.Speed) 
+						local physobj = prop:GetPhysicsObject()
+						local NewVelocity = physobj:GetVelocity()
+						NewVelocity:Normalize()
+						local TargetVelocity = NewVelocity * LastSpeed * 2
+						physobj:SetVelocity(TargetVelocity)
+					end 
+				elseif ent:IsWorld() then 
+				end 
+			end )
+
+			local obj = prop:GetPhysicsObject()
+			obj:SetVelocity(ply:GetForward()*1000)
+		end 
+	end
+end 
+
+function GM:Think()
+	for k,v in pairs(player.GetAll()) do 
+		if v:Alive() and v:KeyDown(IN_ATTACK2) then 
+			if !v.ShieldMade then
+				v.ShieldMade = true 
+				local shield = ents.Create("prop_physics")
+				shield:SetPos(v:EyePos()+v:GetVelocity()*0.1+v:GetForward()*50)
+				shield:SetColor(Color(0, 0, 0))
+				shield:SetAngles(v:GetAngles())
+				shield:SetModel("models/props_interiors/VendingMachineSoda01a_door.mdl")
+				shield:Spawn()
+				shield.MeShield = true 
+				v.Shield = shield 
+			end
+		elseif !v:KeyDown(IN_ATTACK2) and v.ShieldMade then 
+			v.Shield:Remove()
+			v.ShieldMade = false 
+		end 
+		if v.ShieldMade and v.Shield != nil then
+			v.Shield:SetPos(v:EyePos()+v:GetVelocity()*0.1+v:GetForward()*50)
+			v.Shield:SetAngles(v:GetAngles())
+		end
 	end 
 end 
 
